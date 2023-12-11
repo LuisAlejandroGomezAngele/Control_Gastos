3c862bf43a9eeedd7071dac9bf2c0840051ddadf
Luis Gomez <gomezangelesluisalejandro@gmail.com>
2023-12-11 00:10:55 -0600

    Ultimos cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-
+import Header from './componets/Header'
 
 function App() {
-
+  const [presupuesto, setPresupuesto] = useState(0)
   return (
     <>
-      <h1>Control de gastos</h1>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+      />
     </>
   )
 }
--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import NuevoPresupuesto from './NuevoPresupuesto'
 
-export default function Header() {
+export default function Header({ presupuesto, setPresupuesto }) {
   return (
     <div>
+      <header>
+        <h1>Planificador de Gastos</h1>
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+        />
+      </header>
     </div>
   )
 }
