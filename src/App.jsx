91df13dd2d183335b41d1d185820025b848752a7
Luis Gomez <gomezangelesluisalejandro@gmail.com>
2023-12-10 22:39:47 -0600

    Primera fase

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Control de gastos</h1>
+    </>
+  )
+}
+
+export default App
