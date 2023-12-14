a2d35eb11b98532291e82353e813cafc3b083462
FivesLuis <agomez@fivesconsulting.com>
2023-12-13 20:07:32 -0600

    Se agrego el formulario de gastos, el listado de los gastos incluyendo sus descripciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Header from './componets/Header'
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 import Modal from './componets/Modal'
+import { generarID } from './helpers'
+import ListadoGastos from './componets/ListadoGastos'
+
 function App() {
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
   const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+  const [gastos, setGastos] = useState([])
 
   const handleNuevoGasto = () => {
     setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 500);
+  }
+  const guardarGasto = gasto => {
+    gasto.id = generarID()
+    gasto.fecha = Date.now()
+
+    setGastos([...gastos, gasto])
+
+    setAnimarModal(false)
+
+    setTimeout(() => {
+      setModal(false)
+
+    }, 500);
+
   }
   return (
     <>
@@ -20,17 +42,27 @@ function App() {
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
       {isValidPresupuesto ? (
-        <div className='nuevo-gasto'>
-          <img
-            src={IconoNuevoGasto}
-            alt='Icono nuevo gasto'
-            onClick={handleNuevoGasto}
-          />
+        <>
+          <main>
+            <ListadoGastos 
+            gastos={gastos}
+            />
+          </main>
+          <div className='nuevo-gasto'>
+            <img
+              src={IconoNuevoGasto}
+              alt='Icono nuevo gasto'
+              onClick={handleNuevoGasto}
+            />
 
-        </div>
+          </div>
+        </>
       ) : null}
       {modal && <Modal
         setModal={setModal}
+        animarModal={animarModal}
+        setAnimarModal={setAnimarModal}
+        guardarGasto={guardarGasto}
       />}
 
     </>
---
+++ src/componets/Gasto.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { formatearFecha } from '../helpers'
+
+function Gasto({ gasto }) {
+    return (
+        <div className='gasto sombra'>
+            <div className='contenido-gasto'>
+                <div className='descripcion-gasto'>
+                    <p className='categoria'> {gasto.categoria}</p>
+                    <p className='nombre-gasto'>{gasto.nombre}</p>
+                    <p className='fecha-gasto'>
+                        Agregado el: {''}
+                        <span>{formatearFecha(gasto.fecha)}</span>
+                    </p>
+                </div>
+
+            </div>
+            <p className='cantidad-gasto'>${gasto.cantidad}</p>
+
+        </div>
+    )
+}
+
+export default Gasto
